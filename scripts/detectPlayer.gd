ca333ae1271a6d9aa00b41da88de873a80e4080a
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-04-08 11:41:48 +0200

    Add show and hide text in the player detection system

--- scripts/detectPlayer.gd
+++ scripts/detectPlayer.gd
@@ -1,10 +1,12 @@
 extends Area2D
-
+@onready var textbox = $"../CanvasLayer"
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
 		body.in_zone = true
+		textbox.show_text("Press E to use the computer.")
 
 func _on_body_exited(body):
 	if body is CharacterBody2D:
 		body.in_zone = false
+		textbox.hide_textbox()
