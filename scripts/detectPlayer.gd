8df0398dfb79ee1fcaaa0e39917359e46f0d07f1
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-04-08 13:46:54 +0200

    Add new character sprite and textbox assets with import configurations

--- scripts/detectPlayer.gd
+++ scripts/detectPlayer.gd
@@ -1,10 +1,10 @@
 extends Area2D
-@onready var textbox = $"../CanvasLayer"
+@onready var textbox = $"../Textbox"
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
 		body.in_zone = true
-		textbox.show_text("Press E to use the computer.")
+		textbox.show_text("Press E to interact with the computer.")
 
 func _on_body_exited(body):
 	if body is CharacterBody2D:
