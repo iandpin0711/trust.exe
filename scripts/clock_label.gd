749378f639deeeb96bb8bc536b56e7f23bbc46e8
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-04-08 13:06:46 +0200

    Unused Asstets deletede and PC Programs Scenes Created and Designed

--- scripts/clock_label.gd
+++ scripts/clock_label.gd
@@ -2,7 +2,6 @@ extends Label
 
 func _process(_delta):
 	var time = Time.get_time_dict_from_system()
-	
 	var format_string = "%02d:%02d"
 	var actual_time = format_string % [time.hour, time.minute]
 
