75bbeef7d2a78e24737a68c14494ef1b237f29c4
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-04-08 13:46:41 +0200

    Add new scenes for BaseWindow and Contact, and update the WindowXPMainView scene by removing unnecessary references. Modify cursor behavior and optimize the clock script.

--- scripts/clock_label.gd
+++ scripts/clock_label.gd
@@ -2,6 +2,7 @@ extends Label
 
 func _process(_delta):
 	var time = Time.get_time_dict_from_system()
+	
 	var format_string = "%02d:%02d"
 	var actual_time = format_string % [time.hour, time.minute]
 
