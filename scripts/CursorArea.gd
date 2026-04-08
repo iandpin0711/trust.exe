4a529ee0dfd5794c1c0f99bfa61fa58be6200f50
Irene <iandpin0711@g.educaand.es>
2026-04-08 15:00:39 +0200

    Connect interaction areas to their respective scenes

--- scripts/CursorArea.gd
+++ scripts/CursorArea.gd
@@ -5,15 +5,24 @@ var idle: String = "idle"
 
 @export var target_scene: PackedScene
 
+var is_hovering: bool = false
+
 @onready var cursor = get_tree().get_first_node_in_group("Cursor")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Cursor"):
-		body.change_cursor_appearance(hover)
+		is_hovering = true
+		body.change_cursor(hover)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Cursor"):
-		body.change_cursor_appearance(idle)
+		is_hovering = false
+		body.change_cursor(idle)
+
+func _input(event: InputEvent):
+	if is_hovering and event.is_action_pressed("click"):
+		var cursor = get_tree().get_first_node_in_group("Cursor")
+		change_to_scene()
 
 func change_to_scene():
 	if target_scene:
