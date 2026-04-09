8e9d98566e5aba70f253489844c0eeaa5109ccc2
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-04-09 11:57:26 +0200

    TrustMeter Created and Designed

---
+++ scripts/TrustProgressBar.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var progress_bar = $TextureProgressBar
+@onready var percent_label = $TrustPercent
+@export var color_gradient: Gradient
+
+func _process(_delta):
+	var current_val = progress_bar.value
+	percent_label.text = str(int(current_val)) + "%"
+	
+	if color_gradient:
+		var ratio = current_val / progress_bar.max_value
+		var dynamic_color = color_gradient.sample(ratio)
+		percent_label.modulate = dynamic_color
+
+func set_trust_value(value: float):
+	progress_bar.value = value
