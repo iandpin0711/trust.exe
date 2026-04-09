9e1aaca3a44f0af0044c6cff0670e5f42b08f2dc
santiago22 <tasiomartin33@gmail.com>
2026-04-09 14:40:40 +0200

    cursor changed

--- scripts/window_function.gd
+++ scripts/window_function.gd
@@ -11,3 +11,12 @@ func _on_texture_button_2_pressed() -> void:
 
 func _on_texture_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Computer/WindowXPMainView.tscn")
+
+
+
+func _on_button_pressed() -> void:
+	queue_free()
+
+
+func _on_button_3_pressed() -> void:
+	queue_free()
