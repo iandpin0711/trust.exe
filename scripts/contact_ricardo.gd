14f9a1355ac4c63d985d6623228141e7e362a61b
Irene <iandpin0711@g.educaand.es>
2026-04-10 11:39:50 +0200

    Removed duplicate script

--- scripts/contact_ricardo.gd
+++ scripts/contact_ricardo.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
+const DialogueSystemP = preload("res://scenes/DialogueSystem.tscn")
+
+@export var dialogue: Array[DE]
+
 @onready var trust_meter = $ProgressBar 
 
 func _ready():
 	trust_meter.set_trust_value(25.0)
+	
+	var new_dialogue = DialogueSystemP.instantiate()
+	new_dialogue.dialogue = dialogue
+	add_child(new_dialogue)
 
-func _on_correct_option():
-	var new_value = trust_meter.progress_bar.value + 10
-	trust_meter.set_trust_value(new_value)
+func _on_regalo_aceptado():
+	var nuevo_valor = trust_meter.progress_bar.value + 10
+	trust_meter.set_trust_value(nuevo_valor)
