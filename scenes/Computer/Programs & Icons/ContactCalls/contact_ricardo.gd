28afe90a17060063619b4d8951098fbd1e434ae4
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-04-10 10:08:41 +0200

    Victims Scenes Created And Designed

---
+++ scenes/Computer/Programs & Icons/ContactCalls/contact_ricardo.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var trust_meter = $ProgressBar 
+
+func _ready():
+	trust_meter.set_trust_value(25.0)
+
+func _on_regalo_aceptado():
+	var nuevo_valor = trust_meter.progress_bar.value + 10
+	trust_meter.set_trust_value(nuevo_valor)
