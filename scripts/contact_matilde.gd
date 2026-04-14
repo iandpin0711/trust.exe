52926e2c007a54cf0505c8e8157eb7c8ea17c4bb
santiago22 <tasiomartin33@gmail.com>
2026-04-14 14:05:18 +0200

    matilde dialogues and small changue in victories

--- scripts/contact_matilde.gd
+++ scripts/contact_matilde.gd
@@ -4,14 +4,21 @@ extends Node2D
 # Referencia al Label del tiempo (ajusta la ruta si es distinta)
 @onready var timer_label = $TimerLabel 
 
+var trust: float = 50.0
+var dice_roll: int = 0 
+
 func _ready():
-	trust_meter.set_trust_value(40.0)
+	trust_meter.set_trust_value(trust)
 	DialogueManager.show_example_dialogue_balloon(load("res://dialogues/matilde_dialogue.dialogue"), "start")
 	
+func update_trust(amount: float):
+	trust += amount
+	trust = clamp(trust, 0, 100)
+	trust_meter.set_trust_value(trust)
+	print("Trust actual: ", trust)
 
-func _on_correct_option():
-	var new_value = trust_meter.progress_bar.value + 10
-	trust_meter.set_trust_value(new_value)
+func get_neutral_result() -> int:
+	return randi_range(1, 2)
 
 func _on_texture_button_pressed():
 	var tiempo_texto = timer_label.text 
