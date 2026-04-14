488996c267ce73e77f0ce47b87fc719c59c145ed
santiago22 <tasiomartin33@gmail.com>
2026-04-14 12:25:58 +0200

    dialogue system changed

--- scripts/contact_matilde.gd
+++ scripts/contact_matilde.gd
@@ -1,24 +1,13 @@
 extends Node2D
 
-const DialogueSystemP = preload("res://scenes/DialogueSystem.tscn")
-
-@export var dialogue: Array[DE]
-
 @onready var trust_meter = $ProgressBar 
 # Referencia al Label del tiempo (ajusta la ruta si es distinta)
 @onready var timer_label = $TimerLabel 
 
 func _ready():
 	trust_meter.set_trust_value(50.0)
+	DialogueManager.show_example_dialogue_balloon(load("res://dialogues/game_dialogues.dialogue"),"nivel_1")
 	
-	var new_dialogue = DialogueSystemP.instantiate()
-	new_dialogue.dialogue = dialogue
-	add_child(new_dialogue)
-	
-	var screen_size = get_viewport_rect().size
-	var x_pos = (screen_size.x / 2) - (new_dialogue.size.x / 2)
-	var y_pos = screen_size.y * 0.65
-	new_dialogue.position = Vector2(x_pos, y_pos)
 
 func _on_correct_option():
 	var new_value = trust_meter.progress_bar.value + 10
--- scripts/contact_ricardo.gd
+++ scripts/contact_ricardo.gd
@@ -1,23 +1,12 @@
 extends Node2D
 
-const DialogueSystemP = preload("res://scenes/DialogueSystem.tscn")
-
-@export var dialogue: Array[DE]
-
 @onready var trust_meter = $ProgressBar 
 @onready var timer_label = $TimerLabel 
 
 func _ready():
 	trust_meter.set_trust_value(25.0)
 	
-	var new_dialogue = DialogueSystemP.instantiate()
-	new_dialogue.dialogue = dialogue
-	add_child(new_dialogue)
 	
-	var screen_size = get_viewport_rect().size
-	var x_pos = (screen_size.x / 2) - (new_dialogue.size.x / 2)
-	var y_pos = screen_size.y * 0.65
-	new_dialogue.position = Vector2(x_pos, y_pos)
 
 func _on_regalo_aceptado():
 	var new_value = trust_meter.progress_bar.value + 10
