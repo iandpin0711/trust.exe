5484ff9af32475653860755c033835b3245772b6
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-04-14 13:39:25 +0200

    Mr Julian Dialogue Working

--- scripts/contact_mr_julian.gd
+++ scripts/contact_mr_julian.gd
@@ -3,18 +3,24 @@ extends Node2D
 @onready var trust_meter = $ProgressBar 
 @onready var timer_label = $TimerLabel 
 
-func _ready():
-	trust_meter.set_trust_value(15.0)
-	DialogueManager.show_example_dialogue_balloon(load("res://dialogues/julian_dialogue.dialogue"), "start")
-	
+var trust: float = 15.0
+var dice_roll: int = 0 
 
-func _on_correct_option():
-	var new_value = trust_meter.progress_bar.value + 10
-	trust_meter.set_trust_value(new_value)
+func _ready():
+	trust_meter.set_trust_value(trust)
+	DialogueManager.show_example_dialogue_balloon(load("res://dialogues/julian_dialogue.dialogue"), "start")
+
+func update_trust(amount: float):
+	trust += amount
+	trust = clamp(trust, 0, 100)
+	trust_meter.set_trust_value(trust)
+	print("Trust actual: ", trust)
+
+func get_neutral_result() -> int:
+	return randi_range(1, 2)
 
 func _on_texture_button_pressed():
 	var time_text = timer_label.text 
-	var final_trust = trust_meter.progress_bar.value
-	var is_victory = final_trust >= 80.0
-	GlobalData.registrar_llamada("Mr. Julian", time_text, is_victory, final_trust)
+	var is_victory = trust >= 80.0 
+	GlobalData.registrar_llamada("Mr. Julian", time_text, is_victory, trust)
 	get_tree().change_scene_to_file("res://scenes/Computer/Contact.tscn")
