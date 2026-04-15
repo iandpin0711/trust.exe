aefacbce9b0f4a951c01a0bba3a64c0c9deff092
Irene <iandpin0711@g.educaand.es>
2026-04-15 12:35:49 +0200

    FInished Ricardo dialogue

--- scripts/contact_ricardo.gd
+++ scripts/contact_ricardo.gd
@@ -1,24 +1,39 @@
 extends Node2D
 
 @onready var trust_meter = $ProgressBar 
-@onready var timer_label = $TimerLabel
+@onready var timer_label = $TimerLabel 
 
+@export var my_dialogue_resource: DialogueResource 
+@export var dialogue_start_point: String = "start"
+@export var custom_balloon_scene: PackedScene
+
+var trust: float = 20.0
 var dice_roll: int = 0 
 
 func _ready():
-	trust_meter.set_trust_value(25.0)
-	DialogueManager.show_example_dialogue_balloon(load("res://dialogues/ricardo_dialogue.dialogue"), "start")
+	trust_meter.set_trust_value(trust)
+	
+	if custom_balloon_scene:
+		var balloon = custom_balloon_scene.instantiate()
+		get_tree().current_scene.add_child(balloon)
+		balloon.start(my_dialogue_resource, dialogue_start_point)
+	else:
+		DialogueManager.show_example_dialogue_balloon(my_dialogue_resource, dialogue_start_point)
+
+func update_trust(amount: float):
+	trust += amount
+	trust = clamp(trust, 0, 100)
+	trust_meter.set_trust_value(trust)
+	print("Trust actual: ", trust)
 
 func get_neutral_result() -> int:
 	return randi_range(1, 2)
 
-func _on_regalo_aceptado():
-	var new_value = trust_meter.progress_bar.value + 10
-	trust_meter.set_trust_value(new_value)
-
 func _on_texture_button_pressed():
-	var time_text = timer_label.text 
-	var final_trust = trust_meter.progress_bar.value
-	var is_victory = final_trust >= 80.0
-	GlobalData.registrar_llamada("Ricardo", time_text, is_victory, final_trust)
+	var tiempo_texto = timer_label.text 
+	var confianza_final = trust_meter.progress_bar.value
+	var es_victoria = confianza_final >= 80.0
+	
+	GlobalData.registrar_llamada("Matilde", tiempo_texto, es_victoria, confianza_final)
+	
 	get_tree().change_scene_to_file("res://scenes/Computer/Contact.tscn")
