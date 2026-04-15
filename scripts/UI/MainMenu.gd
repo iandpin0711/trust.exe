3269ac081b1aefc300405d826664f8852234b834
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-04-15 11:17:56 +0200

    Updated MainMenu script to play background music

--- scripts/UI/MainMenu.gd
+++ scripts/UI/MainMenu.gd
@@ -1,5 +1,13 @@
 extends Control
 
+@onready var music: AudioStreamPlayer = $AudioStreamPlayer
+
+var MenuMusic = preload("res://assets/sounds/Menu_Controls.mp3")
+
+func _ready() -> void:
+	music.stream = MenuMusic
+	music.play()
+	
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Office.tscn")
 
