c2fa869c3bbf480e640476dd19e29591660d140a
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-04-15 13:19:35 +0200

    Fixed an error with the controls scene audio

--- scripts/Managers/Audio/AudioManager.gd
+++ scripts/Managers/Audio/AudioManager.gd
@@ -1,21 +1,23 @@
 extends Node
 
 var music: AudioStreamPlayer
-var music_scenes = ["MainMenu", "Controls"]
+var music_scenes = ["MainMenu", "Control"]
 
 func _ready() -> void:
 	music = AudioStreamPlayer.new()
 	add_child(music)
 	music.stream = preload("res://assets/sounds/MainMenu.mp3")
+	set_process_unhandled_input(true)
 	get_tree().root.connect("child_entered_tree", _on_scene_changed)
 
 func _on_scene_changed(node: Node) -> void:
 	await get_tree().process_frame
 	var scene_name = get_tree().current_scene.name
+	print("Escena actual: ", scene_name)
 	if scene_name in music_scenes:
+		music.stream_paused = false
 		if not music.playing:
 			music.play()
-		music.stream_paused = false
 	else:
 		music.stop()
 
