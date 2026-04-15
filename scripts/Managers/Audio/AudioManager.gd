df2e1fe516928e97551b61f88d1b7904acb7d68b
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-04-15 12:27:31 +0200

    Fixed an error with the project scenes and updated "stop_music" interaction

--- scripts/Managers/Audio/AudioManager.gd
+++ scripts/Managers/Audio/AudioManager.gd
@@ -1,16 +1,24 @@
 extends Node
 
-var music : AudioStreamPlayer
+var music: AudioStreamPlayer
+var music_scenes = ["MainMenu", "Controls"]
 
 func _ready() -> void:
 	music = AudioStreamPlayer.new()
 	add_child(music)
 	music.stream = preload("res://assets/sounds/Menu_Controls.mp3")
-	music.play()
+	get_tree().root.connect("child_entered_tree", _on_scene_changed)
+
+func _on_scene_changed(node: Node) -> void:
+	await get_tree().process_frame
+	var scene_name = get_tree().current_scene.name
+	if scene_name in music_scenes:
+		if not music.playing:
+			music.play()
+		music.stream_paused = false
+	else:
+		music.stop()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("stop_music"):
-		if music.playing:
-			music.stop()
-		else:
-			music.play()
+		music.stream_paused = not music.stream_paused
